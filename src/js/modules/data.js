70b50929cab5d42c64812045e8fd5f6e2cb54e26
Abdulrahman <rahhal.abdelrahman@gmail.com>
2023-04-17 06:23:54 +0300

    Add data.js to handle tasks and projects

---
+++ src/js/modules/data.js
@@ -0,0 +1,75 @@
+import { v4 as uuidv4 } from 'uuid';
+import { isValid } from 'date-fns';
+
+class Storage {
+  static getTasks() {
+    return localStorage.getItem('tasks')
+      ? JSON.parse(localStorage.getItem('tasks'))
+      : [];
+  }
+
+  static updateTasks(tasks) {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
+
+  static getProjects() {
+    return localStorage.getItem('projects')
+      ? JSON.parse(localStorage.getItem('projects'))
+      : [];
+  }
+
+  static updateProjects(projects) {
+    localStorage.setItem('projects', JSON.stringify(projects));
+  }
+}
+
+class Task {
+  constructor(title, desc, priority, dueDate, projectId) {
+    this.title = typeof title === 'string' ? title.trim() : '';
+    this.desc = typeof desc === 'string' ? desc.trim() : '';
+    this.priority = typeof priority === 'string' ? priority.trim() : '';
+    this.dueDate = isValid(dueDate) ? dueDate : new Date();
+    this.projectId = typeof projectId === 'string' ? projectId.trim() : '';
+    this.id = uuidv4();
+    this.isCompleted = false;
+  }
+
+  update(title, desc, priority, dueDate, projectId) {
+    this.title = typeof title === 'string' ? title.trim() : this.title;
+    this.desc = typeof desc === 'string' ? desc.trim() : this.desc;
+    this.priority =
+      typeof priority === 'string' ? priority.trim() : this.priority;
+    this.dueDate = isValid(dueDate) ? dueDate : this.dueDate;
+    this.projectId = typeof projectId === 'string' ? projectId : this.projectId;
+  }
+
+  toggleStatus() {
+    this.isCompleted = !this.isCompleted;
+  }
+}
+
+class Project {
+  constructor(title) {
+    this.title = title;
+    this.id = uuidv4();
+    this.tasks = [];
+  }
+
+  update(title) {
+    this.title = title;
+  }
+
+  addTask(task) {
+    this.tasks.push(task);
+  }
+
+  removeTask(taskId) {
+    this.tasks = this.tasks.filter((task) => task.id !== taskId);
+  }
+
+  getTasks() {
+    return this.tasks;
+  }
+}
+
+export { Storage, Task, Project };
