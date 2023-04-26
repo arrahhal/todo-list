6a41d9474d7b1c64a954391997fc7b5a7c54ec50
Abdulrahman <rahhal.abdelrahman@gmail.com>
2023-04-26 11:02:19 +0300

    save changes with _commit()

--- src/js/modules/app.js
+++ src/js/modules/app.js
@@ -1,121 +1,205 @@
 import { isThisWeek, isToday } from 'date-fns';
 import { Task, Project, Storage } from './data';
 
-function addDefaultProjectIfNone() {
-  if (!Storage.getProjects()?.length) {
-    const inbox = new Project('Inbox');
-    Storage.updateProjects([inbox]);
-  }
-}
+let projects = Storage.getProjects();
 
-class TaskManager {
-  constructor() {
-    addDefaultProjectIfNone();
-
-    this.projects = Storage.getProjects();
-    this.commit = () => Storage.updateProjects(this.projects);
-  }
-
-  findProjectById(projectId) {
-    return this.projects.find((project) => project.id === projectId);
-  }
-
-  removeProject(projectId) {
-    const projectIndex = this.projects.findIndex(
-      (project) => project.id === projectId
+const addDefaultProjectIfNone = () => {
+  if (!projects.length) {
+    const inbox = Project('Inbox');
+    const todayTask = Task(
+      'today Task',
+      'description',
+      'low',
+      new Date(2023, 3, 26, 10, 15),
+      inbox.id
     );
-    if (projectIndex > -1) {
-      this.projects.splice(projectIndex, 1);
-      this.commit();
-    }
+    console.log(todayTask);
+    todayTask.update(
+      'new title',
+      'descriptNew',
+      'medium',
+      new Date(2023, 3, 26),
+      inbox.id
+    );
+    //!! there is a issue here the todayTask never updated correctly
+    console.log(todayTask);
+    projects.push(inbox);
+    inbox.addTask(todayTask);
+    _commit();
   }
+};
 
-  updateProject(newTitle, projectId) {
-    const project = this.findProjectById(projectId);
+const findProjectById = (projectId) =>
+  projects.find((project) => project.id === projectId);
+
+const removeProject = (projectId) => {
+  const projectIndex = projects.findIndex(
+    (project) => project.id === projectId
+  );
+  if (projectIndex > -1) {
+    projects.splice(projectIndex, 1);
+    _commit();
+  }
+};
+
+const updateProject = (newTitle, projectId) => {
+  const project = findProjectById(projectId);
+  if (project) {
+    project.title = newTitle;
+    _commit();
+  }
+};
+
+const createTask = (title, desc, priority, dueDate, projectId) => {
+  const task = new Task(title, desc, priority, dueDate, projectId);
+  const project = findProjectById(projectId);
+  if (project) {
+    project.addTask(task);
+    _commit();
+  }
+};
+
+const getAllTasks = () => projects.flatMap((project) => project.getTasks());
+
+const findTaskIndex = (taskId) =>
+  getAllTasks().findIndex((task) => task.id === taskId);
+
+const removeTask = (taskId) => {
+  const taskIndex = findTaskIndex(taskId);
+  if (taskIndex > -1) {
+    const task = getAllTasks()[taskIndex];
+    const project = findProjectById(task.projectId);
     if (project) {
-      project.title = newTitle;
-      this.commit();
+      project.removeTask(taskId);
+      _commit();
     }
   }
+};
 
-  createTask(title, desc, priority, dueDate, projectId) {
-    const task = new Task(title, desc, priority, dueDate, projectId);
-    const project = this.findProjectById(projectId);
-    if (project) {
-      project.addTask(task);
-      this.commit();
-    }
+const toggleTaskStatus = (taskId) => {
+  const taskIndex = findTaskIndex(taskId);
+  if (taskIndex > -1) {
+    const task = getAllTasks()[taskIndex];
+    task.toggleStatus();
+    _commit();
   }
+};
 
-  getAllTasks() {
-    return this.projects.flatMap((project) => project.getTasks());
+const getTask = (taskId) => {
+  const taskIndex = findTaskIndex(taskId);
+  if (taskIndex > -1) {
+    return getAllTasks()[taskIndex];
   }
+  return null;
+};
 
-  findTaskIndex(taskId) {
-    return this.getAllTasks().findIndex((task) => task.id === taskId);
+const updateTask = (
+  newTitle,
+  newDesc,
+  newPriority,
+  newDate,
+  newProjectId,
+  taskId
+) => {
+  const taskIndex = findTaskIndex(taskId);
+  if (taskIndex > -1) {
+    const task = getAllTasks()[taskIndex];
+    task.update(newTitle, newDesc, newPriority, newDate, newProjectId);
+    _commit();
   }
+};
 
-  removeTask(taskId) {
-    const taskIndex = this.findTaskIndex(taskId);
-    if (taskIndex > -1) {
-      const task = this.getAllTasks()[taskIndex];
-      const project = this.findProjectById(task.projectId);
-      if (project) {
-        project.removeTask(taskId);
-        this.commit();
-      }
-    }
+const getTodayTasks = () =>
+  getAllTasks().filter((task) => isToday(task.dueDate));
+
+const getThisWeekTasks = () =>
+  getAllTasks().filter((task) => isThisWeek(task.dueDate));
+
+const getCompletedTasks = () =>
+  getAllTasks().filter((task) => task.isCompleted);
+
+const getProjectTasks = (projectId) => {
+  const project = findProjectById(projectId);
+  if (project) {
+    return project.getTasks();
   }
+  return [];
+};
 
-  toggleTaskStatus(taskId) {
-    const taskIndex = this.findTaskIndex(taskId);
-    if (taskIndex > -1) {
-      const task = this.getAllTasks()[taskIndex];
-      task.toggleStatus();
-      this.commit();
-    }
-  }
+const getProjects = () => projects;
 
-  getTask(taskId) {
-    const taskIndex = this.findTaskIndex(taskId);
-    if (taskIndex > -1) {
-      return this.getAllTasks()[taskIndex];
-    }
-    return null;
-  }
+const createTaskFunc = (title, desc, priority, dueDate, projectId) => {
+  createTask(title, desc, priority, dueDate, projectId);
+};
 
-  updateTask(newTitle, newDesc, newPriority, newDate, newProjectId, taskId) {
-    const taskIndex = this.findTaskIndex(taskId);
-    if (taskIndex > -1) {
-      const task = this.getAllTasks()[taskIndex];
-      task.updateTask(newTitle, newDesc, newPriority, newDate, newProjectId);
-      this.commit();
-    }
-  }
+const updateTaskFunc = (
+  newTitle,
+  newDesc,
+  newPriority,
+  newDate,
+  newProjectId,
+  taskId
+) => {
+  updateTask(newTitle, newDesc, newPriority, newDate, newProjectId, taskId);
+};
 
-  getTodayTasks() {
-    return this.getAllTasks().filter((task) => isToday(task.dueDate));
-  }
+const removeTaskFunc = (taskId) => {
+  removeTask(taskId);
+};
 
-  getThisWeekTasks() {
-    return this.getAllTasks().filter((task) => isThisWeek(task.dueDate));
-  }
+const toggleTaskStatusFunc = (taskId) => {
+  toggleTaskStatus(taskId);
+};
 
-  getCompletedTasks() {
-    return this.getAllTasks().filter((task) => task.isCompleted);
-  }
+const getTaskFunc = (taskId) => {
+  return getTask(taskId);
+};
 
-  getProjectTasks(projectId) {
-    const project = this.findProjectById(projectId);
-    if (project) {
-      return project.getTasks();
-    }
-    return [];
-  }
+const getAllTasksFunc = () => {
+  return getAllTasks();
+};
 
-  getProjects() {
-    return this.projects;
-  }
-}
+const getTodayTasksFunc = () => {
+  return getTodayTasks();
+};
 
-export default TaskManager;
+const getThisWeekTasksFunc = () => {
+  return getThisWeekTasks();
+};
+
+const getCompletedTasksFunc = () => {
+  return getCompletedTasks();
+};
+
+const getProjectTasksFunc = (projectId) => {
+  return getProjectTasks(projectId);
+};
+
+const addProject = (title) => {
+  const project = new Project(title);
+  projects.push(project);
+  _commit();
+};
+
+const _commit = () => {
+  Storage.updateProjects(projects);
+};
+
+addDefaultProjectIfNone();
+
+export const taskManager = {
+  createTaskFunc,
+  updateTaskFunc,
+  removeTaskFunc,
+  toggleTaskStatusFunc,
+  getTaskFunc,
+  getAllTasksFunc,
+  getTodayTasksFunc,
+  getThisWeekTasksFunc,
+  getCompletedTasksFunc,
+  getProjectTasksFunc,
+  addProject,
+  removeProject,
+  updateProject,
+  getProjects,
+};
